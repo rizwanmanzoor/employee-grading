0ba10a407924cd9b67e5d20cdcfa0041824d2b16
rizwan manzoor <rizwanmanzoor200@gmail.com>
2025-10-28 19:55:44 +0500

    file upload changes

--- src/components/steps/Step1.jsx
+++ src/components/steps/Step1.jsx
@@ -1,10 +1,21 @@
-import { BookOpen } from "lucide-react";
+import { useState } from "react"
+;
+import { Input } from "../ui/input";
 import { Label } from "@/components/ui/label";
+import { BookOpen, Paperclip } from "lucide-react";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import SelectVerifiedGroup from "../selectVerifiedGroup/SelectVerifiedGroup";
 import SelectRelevantGroup from "../selectRelevantGroup/SelectRelevantGroup";
 
 const Step1 = () => {
+  const [fileName, setFileName] = useState("");
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFileName(file.name);
+    }
+  };
   return (
     <>
       <div className="flex items-center gap-3 pb-4 border-b">
@@ -55,6 +66,45 @@ const Step1 = () => {
             </Label>
           </div>
         </RadioGroup>
+
+        <div
+          className="input-field inline-flex items-center p-1 min-w-xl w-max overflow-hidden my-5"
+          style={{
+            backgroundColor: "var(--card)",
+            color: "var(--foreground)",
+          }}
+        >
+          <div className="px-4 flex items-center">
+            <Paperclip size={"18"} />
+            <p className="text-sm ml-3">
+              {fileName ? fileName : "No file selected"}
+            </p>
+          </div>
+
+          <label
+            htmlFor="uploadFile"
+            className="text-sm font-medium px-3 py-2.5 outline-none rounded-md cursor-pointer ml-auto w-max block transition-colors"
+            style={{
+              backgroundColor: "var(--primary)",
+              color: "var(--primary-foreground)",
+            }}
+            onMouseEnter={(e) =>
+              (e.currentTarget.style.backgroundColor = "var(--secondary)")
+            }
+            onMouseLeave={(e) =>
+              (e.currentTarget.style.backgroundColor = "var(--primary)")
+            }
+          >
+            Upload File
+          </label>
+
+          <Input
+            type="file"
+            id="uploadFile"
+            className="hidden"
+            onChange={handleFileChange}
+          />
+        </div>
       </div>
 
       <div className="flex flex-col items-end gap-4 flex-wrap">
